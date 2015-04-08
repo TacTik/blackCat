ac00403819d60aa7e5a38bafac28cb7184835571
julietteTaka <juliettetuxbelin@gmail.com>
2015-04-08 18:13:46 +0200

    game : menu on the main window. better indent.

--- src/fr/tactik/game/GameWindow.java
+++ src/fr/tactik/game/GameWindow.java
@@ -4,22 +4,25 @@ import javax.swing.JFrame;
 
 /**
  * 
- * @author juliette
  * The main window. Contains the whole universe.
  *
  */
 public class GameWindow extends JFrame{
 
-	//menu
+	Menu gameMenu = new Menu();
 	//hero
 	//monde
 	//controler de jeu
 	//controler de menu
 
 	GameWindow(){
-	setTitle("Un super Titre de Jeu OH YEAH");
-	setSize(800,800);
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setVisible (true);		
+		setTitle("Un super Titre de Jeu OH YEAH");
+		setSize(800,800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Menu gameMenu = new Menu();
+		setJMenuBar(gameMenu);
+		
+		setVisible (true);		
 	}
 }
