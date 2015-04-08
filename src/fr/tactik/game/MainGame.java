7352ac1534097ea78bb88aadf9eb4ab7e31dbe2a
julietteTaka <juliettetuxbelin@gmail.com>
2015-04-08 17:55:54 +0200

    Game : The main function. Launches the game.

---
+++ src/fr/tactik/game/MainGame.java
@@ -0,0 +1,13 @@
+package fr.tactik.game;
+
+/**
+ * 
+ * @author juliette
+ * The main function. Create the window that launches the game.
+ *
+ */
+public class MainGame {
+	public static void main (String [] args){
+		GameWindow window = new GameWindow();
+	}
+}
