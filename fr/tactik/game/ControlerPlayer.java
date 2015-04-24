e16512d47fbceb33c9c66962a779163021418c3b
julietteTaka <juliettetuxbelin@gmail.com>
2015-04-24 18:08:10 +0200

    game : better way to control player.

--- fr/tactik/game/ControlerPlayer.java
+++ fr/tactik/game/ControlerPlayer.java
@@ -6,33 +6,26 @@ import java.util.HashMap;
 
 public class ControlerPlayer implements KeyListener {
 	Player player;
-	HashMap<Integer, Boolean> keys = new HashMap<Integer, Boolean>();
-
-	
 	public ControlerPlayer(Player player) {
 		this.player = player;
-		keys.put(KeyEvent.VK_UP, false);
-		keys.put(KeyEvent.VK_RIGHT, false);
-		keys.put(KeyEvent.VK_DOWN, false);
-		keys.put(KeyEvent.VK_LEFT, false);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e){
 
-		keys.put(e.getKeyCode(), true);
+		int key = e.getKeyCode();
 		
-		if(keys.get(KeyEvent.VK_LEFT)){
-			player.moveLeft(3);
+		if(key == KeyEvent.VK_LEFT){
+			player.moveLeft(4);
 	    }
-		if(keys.get(KeyEvent.VK_RIGHT)){
-			player.moveRight(3);
+		if(key == KeyEvent.VK_RIGHT){
+			player.moveRight(4);
 	    }
-		if(keys.get(KeyEvent.VK_UP)){
-			player.moveUp(3);
+		if(key == KeyEvent.VK_UP){
+			player.moveUp(4);
 	    }
-		if(keys.get(KeyEvent.VK_DOWN)){
-			player.moveDown(3);
+		if(key == KeyEvent.VK_DOWN){
+			player.moveDown(4);
 	    }
 	    
 	}
@@ -40,28 +33,21 @@ public class ControlerPlayer implements KeyListener {
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		keys.put(e.getKeyCode(), false);
+		int key = e.getKeyCode();
 		
-		if(false == keys.get(KeyEvent.VK_LEFT)){
+		if(key == KeyEvent.VK_LEFT){
 			player.xspeed = 0;
-			System.out.println("LEfT");
-
 	    }
-		if(!keys.get(KeyEvent.VK_RIGHT)){
+		if(key == KeyEvent.VK_RIGHT){
 			player.xspeed = 0;
-			System.out.println("right");
-
 	    }
-		if(!keys.get(KeyEvent.VK_UP)){
+		if(key == KeyEvent.VK_UP){
 			player.yspeed = 0;
-			System.out.println("up");
-
 	    }
-		if(!keys.get(KeyEvent.VK_DOWN)){
+		if(key == KeyEvent.VK_DOWN){
 			player.yspeed = 0;
-			System.out.println("Down");
-
 	    }
+
 	}
 
 	@Override
