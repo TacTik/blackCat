d0fdec849bcdc3a4b08d9bf4fca66de1f02fe103
julietteTaka <juliettetuxbelin@gmail.com>
2015-04-27 16:59:26 +0200

    editor : display the background layer, colors only.

--- fr/tactik/editor/LevelPanel.java
+++ fr/tactik/editor/LevelPanel.java
@@ -1,8 +1,12 @@
 package fr.tactik.editor;
 
+import handlers.TextureHandler;
+
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -11,6 +15,9 @@ import javax.swing.JScrollPane;
 public class LevelPanel extends JScrollPane{
 	private static final long serialVersionUID = 1L;
 	private static JPanel levelView;
+	int width, height;
+	
+	Image backgroundImage;
 	//TODO : afficher tiles / plateform / objects delimitations ?
 	
 	public LevelPanel(int vsbPolicy, int hsbPolicy) {
@@ -18,18 +25,28 @@ public class LevelPanel extends JScrollPane{
 	}
 
 	
-	public void initView() {
-		JPanel outer = new JPanel(); //permet d'avoir le JPanel à la bonne taille, même si plus petit que le scrollPanel
+	public int initView(int width, int height, String background) {
+		backgroundImage = TextureHandler.getImageFromPath(background);
+		if (null == backgroundImage)
+			return -1;
+		
+		JPanel outer = new JPanel();
 		levelView = new JPanel();
 		outer.add(levelView);
 		
-		//TODO think about define a level model to get width heigh etc
-		levelView.setPreferredSize(new Dimension((int)800, (int)500));
+		this.width = width;
+		this.height = height;
+		levelView.setPreferredSize(new Dimension((int)width, (int)height));
+		levelView.setBackground(Color.white);
+		outer.setBackground(Color.gray);
+		
 		getViewport().add(outer, BorderLayout.NORTH);
 		revalidate();
+		return 1;
 	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		g.drawImage(backgroundImage,0,0,null);
 	}
 }
