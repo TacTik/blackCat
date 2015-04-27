3fcbe8b218f907a13803dd39f255b62b2ba31049
julietteTaka <juliettetuxbelin@gmail.com>
2015-04-27 16:55:13 +0200

    handler : texture handler

---
+++ handlers/TextureHandler.java
@@ -0,0 +1,47 @@
+package handlers;
+
+import java.awt.Dialog;
+import java.awt.Image;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+public class TextureHandler {
+
+	private TextureHandler() {}
+	public static Vector<Image> getTexturesFromFolder(String path, int... number ){
+		Vector<Image> texturesImages = new Vector<Image>();
+		
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles();
+		
+		int max = (number.length == 0) ? listOfFiles.length : number[0] + 1;
+		
+		for (int i = 0; i < max; i++) {
+			if (listOfFiles[i].isFile()) {
+				try {
+					texturesImages.add (ImageIO.read(new File(path + listOfFiles[i].getName())));
+				} catch (IOException e) {
+					System.out.println("expect");
+					e.printStackTrace();
+				}
+			}
+		} 
+		return texturesImages;
+	}
+	
+	public static Image getImageFromPath(String path){
+		Image image = null;
+		try {
+			image = ImageIO.read(new File(path));
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "can't read your file !");
+		}
+		return image;
+	}
+
+}
