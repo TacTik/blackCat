eab959b582c1346fdbabaf6f134d27388676dca3
julietteTaka <juliettetuxbelin@gmail.com>
2015-04-29 18:16:35 +0200

    delete texture loader function, have a file dedicated

--- fr/tactik/editor/TilesPanel.java
+++ fr/tactik/editor/TilesPanel.java
@@ -1,64 +1,43 @@
 package fr.tactik.editor;
 
-import handlers.TextureHandler;
-
-import javax.swing.ImageIcon;
+import handlers.TexturesMapper;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.util.Iterator;
 import java.util.Vector;
 
 public class TilesPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
+	private int nbTexturesSet = 6; // BEGIN AT 1 ! so, there is 5 texture in it.
 	static String rootdir = System.getProperty("user.dir");
 	
-	ImageIcon player_texturesImages;
-	ImageIcon still_1_texturesImages;
-	ImageIcon still_2_texturesImages;
-	ImageIcon mobile_1_texturesImages;
-	ImageIcon mobile_2_texturesImages;
-	
-
-	
 	public TilesPanel() {
+		boolean t = TexturesMapper.init();
+
 		Vector <JButton> buttons = new Vector<JButton>();
 		setBackground(Color.white);
 		FlowLayout layout = new FlowLayout(0, 0, 0);
 		setLayout(layout);
-
-		player_texturesImages = TextureHandler.getImageIconFromPath(rootdir + "/images/game/player/", 1).elementAt(0);
-		still_1_texturesImages = TextureHandler.getImageIconFromPath(rootdir + "/images/game/still1/", 1).elementAt(0);
-		still_2_texturesImages = TextureHandler.getImageIconFromPath(rootdir + "/images/game/still2/", 1).elementAt(0);
-		mobile_1_texturesImages = TextureHandler.getImageIconFromPath(rootdir + "/images/game/mobile1/", 1).elementAt(0);
-		mobile_2_texturesImages = TextureHandler.getImageIconFromPath(rootdir + "/images/game/mobile2/", 1).elementAt(0);
 		
-		buttons.add(new JButton (player_texturesImages));
-		buttons.add(new JButton (still_1_texturesImages));
-		buttons.add(new JButton (still_1_texturesImages));
-		buttons.add(new JButton (mobile_1_texturesImages));
-		buttons.add(new JButton (mobile_2_texturesImages));
+		for (int i = 1; i <= nbTexturesSet; i++)
+		buttons.add(new JButton (TexturesMapper.getImageById(i)));
 		
 
 		for (JButton b : buttons) {
 			b.setPreferredSize(new Dimension(50, 50));
+			b.setBackground(null);
 	        add(b);
 		  }
 
         
        // b.addActionListener(new ButtonEvent(b, system, row, col));
-
 	}
 
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		setPreferredSize(new Dimension (100, 200));
-   	
 	}
 }
