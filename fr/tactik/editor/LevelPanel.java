3acb6be32e870d9168158e8b39007ca531355ffe
julietteTaka <juliettetuxbelin@gmail.com>
2015-05-06 10:34:47 +0200

    editor minor fixes

--- fr/tactik/editor/LevelPanel.java
+++ fr/tactik/editor/LevelPanel.java
@@ -122,11 +122,9 @@ public class LevelPanel extends JPanel implements MouseListener, MouseMotionList
 	
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		if(e.getX() < 0 || e.getY() > height || e.getX() > width || e.getY() < 0){
+		if(e.getX() < 0 || e.getY() > height || e.getX() > width || e.getY() < 0)
 			return;
-		}
 			
-		
 		if (SwingUtilities.isRightMouseButton(e)){
 			tiles[ e.getX() / 50][e.getY() / 50] = 0;
 		}
@@ -137,7 +135,7 @@ public class LevelPanel extends JPanel implements MouseListener, MouseMotionList
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if(e.getX() < 0 || e.getY() > height || e.getX() > width || e.getY() < 0){
+		if(e.getX() < 0 || e.getY() > height || e.getX() > width || e.getY() < 0)
 			return;
 		
 		if (SwingUtilities.isRightMouseButton(e)){
@@ -145,6 +143,7 @@ public class LevelPanel extends JPanel implements MouseListener, MouseMotionList
 		}
 		else 
 			tiles[ e.getX() / 50][e.getY() / 50] = SelectTileHelper.getSelectedTile();
+		
 		repaint();
 	}
 	
